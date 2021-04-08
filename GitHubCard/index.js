ef75561c93fdc3ba4a78f71670e26f31a17de25f
Connor Rayburn <connorrayburn.work@gmail.com>
2021-04-08 16:20:32 -0700

    created elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,6 +6,22 @@ import axios from 'axios';
     https://api.github.com/users/<your name>
 */
 
+const result = axios.get('https://api.github.com/users/crayburn18');
+// console.log(result);
+
+const cardData = document.querySelector('.cards');
+
+axios
+  .get('https://api.github.com/users/crayburn18')
+  .then(res => {
+    const card = cardMaker(res.data);
+    cardData.appendChild(card);
+    console.log(res);
+  })
+  .catch(drama => {
+    console.log(drama);
+  })
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -51,6 +67,50 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker(user){
+  const {} = user;
+
+  const userCard = document.createElement('div');
+  const userImg = document.createElement('img');
+  const userInfo = document.createElement('div');
+  const userName = document.createElement('h3');
+  const userUserName = document.createElement('p');
+  const userLocation = document.createElement('p');
+  const userProfile = document.createElement('p');
+  const userGitLink = document.createElement('a');
+  const userFollowers = document.createElement('p');
+  const userFollowing = document.createElement('p');
+  const userBio = document.createElement('p');
+
+  userCard.classList.add('card');
+  userImg.src = '';
+  userInfo.classList.add('card-info');
+  userName.classList.add('name');
+  userName.textContent = '';
+  userUserName.classList.add('username');
+  userUserName.textContent = '';
+  userLocation.textContent = '';
+  userProfile.textContent = 'Profile: ';
+  userGitLink.href = '';
+  userGitLink.textContent = 'Git Hub';
+  userFollowers.textContent = 'Followers: ';
+  userFollowing.textContent = 'Following: ';
+  userBio.textContent = '';
+
+  userCard.appendChild(userImg);
+  userCard.appendChild(userInfo);
+  userInfo.appendChild(userName);
+  userInfo.appendChild(userUserName);
+  userInfo.appendChild(userLocation);
+  userInfo.appendChild(userProfile);
+  userProfile.appendChild(userGitLink);
+  userInfo.appendChild(userFollowers);
+  userInfo.appendChild(userFollowing);
+  userInfo.appendChild(userBio);
+  
+  return cardMaker;
+}
 
 /*
   List of LS Instructors Github username's:
